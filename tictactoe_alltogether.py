530e839ad9c961abd4d2e9edc36c25725261e199
Koenel <k20111151@kcl.ac.uk>
2024-06-30 23:50:50 +0200

    import random module to make computer moves work

--- tictactoe_alltogether.py
+++ tictactoe_alltogether.py
@@ -1,3 +1,4 @@
+import random
 start_board = "--------------------"
 player_user = "x"
 player_computer = "o"
@@ -13,7 +14,7 @@ def user_board(board):
     while True:
         mark = player_user
         position = int(input("Which position do you want to play? Choose a number from 0-19! "))
-        if position > 19 or board[position] != "-" :
+        if position > 19 or board[position] != "-" : # this is a negative positive to get the expected return, see computer move
             print("This is no valid position. Try again.")
         else:
             return position
@@ -31,6 +32,14 @@ while "-" in start_board:
     user_move = user_board(start_board)
     start_board = update_board(start_board, user_move, player_user)
     print(f"Here's the new board: {start_board}")
+    computer_move = computer_board(start_board)
+    start_board = update_board(start_board, computer_move, player_computer)
+    print(f"The computer played. The new board is {start_board}.")
+    if "-" not in start_board:
+        print("Game over.")
+        break
+    
+    
 
 #while game_is_player:
      #printboard(start_board)
