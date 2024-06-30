0f4a62abae87d3ea90de2355182ebd23fa2994d3
Koenel <k20111151@kcl.ac.uk>
2024-06-30 23:38:32 +0200

    split update and user input

--- tictactoe_alltogether.py
+++ tictactoe_alltogether.py
@@ -9,20 +9,26 @@ def printboard(board):
 
 printboard(start_board) # checks that the board is working
 
-def update_board(board):
+def user_board(board):
     while True:
         mark = player_user
         position = int(input("Which position do you want to play? Choose a number from 0-19! "))
         if position > 19 or board[position] != "-" :
             print("This is no valid position. Try again.")
         else:
-            print(board[:position] + mark + board[position+1:])
+            return position
 
+def update_board(board, position, mark):
+    return board[:position] + mark + board[position+1:]
 
+while "-" in start_board:
+    user_move = user_board(start_board)
+    start_board = update_board(start_board, user_move, player_user)
+    print(f"Here's the new board: {start_board}")
 
-while game_is_player:
-     printboard(start_board)
-     update_board(start_board)
+#while game_is_player:
+     #printboard(start_board)
+     #update_board(start_board)
 
 
 #while index < len(start_board):
